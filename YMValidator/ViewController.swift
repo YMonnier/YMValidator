1dce7ba437ebba31d5ff7e788f64a7d0905fc523
Ysee Monnier <yseemonnier@gmail.com>
2016-08-26 23:20:09 +0200

    init project

---
+++ YMValidator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YMValidator
+//
+//  Created by Ysée MONNIER on 26/08/16.
+//  Copyright © 2016 Ysée MONNIER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

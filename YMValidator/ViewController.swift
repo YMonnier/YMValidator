0f3fdab81449a328bbfe20e7e28cd43b31c0b7e5
Ysee Monnier <yseemonnier@gmail.com>
2016-08-27 02:06:44 +0200

    static funtion Validates which checks all inputs

--- YMValidator/ViewController.swift
+++ YMValidator/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputField: YMValidator!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.errorLabel.text = ""
+        YMValidator.validates(self)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
--- YMValidator/YMValidator/YMValidator.swift
+++ YMValidator/YMValidator/YMValidator.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 
-private class EmailValidator: Validator {
-    private var regex: String! = ""
+@objc(EmailValidator)
+private class EmailValidator: NSObject, Validator {
+    private var regex: String = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+    //private var error: String = "Incorrect email."
 }
 
 private protocol Validator {
-    var regex: String! { get set }
+    var regex: String { get set }
+    //var error: String { get set }
 }
 
 private extension NSObject {
+    /**
+     Reflection function which allows to create swift class from class name.
+     - Parameter className: The instance class name.
+     - Returns: Nil or Validator instance.
+    */
     class func fromClassName(className : String) -> Validator? {
         let aClass = NSClassFromString(className) as? NSObject.Type
         if let object = aClass {
@@ -29,17 +37,19 @@ private extension NSObject {
 }
 
 @IBDesignable
-class YMValidator: UITextField {
+public class YMValidator: UITextField {
     
     /// Validator class name.
-    @IBInspectable var Class: String = "" {
-        didSet {
-            self.engin()
-        }
-    }
+    @IBInspectable var Class: String = ""
+    
+    /// Error message.
+    @IBInspectable var Error: String = ""
     
     /// Regex string.
     private var regex: String?
+    
+    /// Error label.
+    private var errorLabel: UILabel?
     
     /**
      Returns an object initialized from data in a given unarchiver. 
@@ -47,7 +57,7 @@ class YMValidator: UITextField {
      - Parameter decoder: An unarchiver object.
      - Returns: self, initialized using the data in decoder.
     */
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.setupView()
     }
@@ -64,22 +74,79 @@ class YMValidator: UITextField {
     }
     
     private func setupView() {
+        self.delegate = self
         self.addTarget(self, action: #selector(YMValidator.textFieldDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
     }
     
-    private func setupValidator() {
-        let Class = NSObject.fromClassName(self.Class)
-        self.regex = Class?.regex ?? nil
+    internal func updateView(error: Bool = false) {
+        if error {
+            self.errorLabel?.text = self.Error
+        } else {
+            self.errorLabel?.text = ""
+        }
     }
     
-    func engin() {
+    private func parseValidator() {
+        print(#function + " -- " + self.Class)
         
+        let Class = NSObject.fromClassName(self.Class)
+        print(Class)
+        self.regex = Class?.regex ?? nil
+        
+    }
+    
+    internal func validate() -> Bool {
+        return self.engin(self.text!)
+    }
+    
+    private func engin(text: String) -> Bool{
+        print(#function)
+        print(self.Class)
+        print(self.regex)
+        if let regex = self.regex {
+            let predicate = NSPredicate(format:"SELF MATCHES %@", regex)
+            return predicate.evaluateWithObject(text)
+            
+        }
+        return false
     }
     
     func textFieldDidChange(textField: UITextField) {
         print(#function)
         if let text = textField.text {
-            
+            if !self.engin(text) {
+                self.errorLabel?.text = self.Error
+            } else {
+                self.errorLabel?.text = ""
+            }
         }
+    }
+    
+    class func validates(controller: UIViewController) -> Bool {
+        let subViews = controller.view.subviews
+        let customTextFields = subViews.filter { (view) -> Bool in
+            view is YMValidator
+        }
+        var count = 0
+        for tf in customTextFields as! [YMValidator] {
+            if !tf.validate() {
+                count += 1
+                
+            }
+        }
+        return true
+    }
+}
+
+extension YMValidator {
+    public func setErrorLabel(label: UILabel) {
+        self.errorLabel = label
+    }
+}
+
+extension YMValidator: UITextFieldDelegate {
+    //MARK: UITextFieldDelegate
+    public func textFieldDidBeginEditing(textField: UITextField) {
+        self.parseValidator()
     }
 }
